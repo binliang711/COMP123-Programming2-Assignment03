4438e4cb637ea6a47bd3e1d3e386a6cd8cf5e9a5
Bin Liang <bingliang711@hotmail.com>
2015-02-24 07:51:50 -0700

    Finished Abstract Class Planet.

---
+++ COMP123-Programming2-Assignment03/COMP123-Programming2-Assignment03/Planet.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Programming2_Assignment03
+{
+    abstract class Planet
+    {
+        // private instance variables ++++++++++++++++++++++++++++++++++++
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        //public properties +++++++++++++++++++++++++++++++++++++++++++
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter; //Read-only
+            }
+        }
+
+        public double Mass
+        {
+            get
+            {
+                return this._mass; //Read-only
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name; //Read-only
+            }
+        }
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+        // CONSTRUCTORS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        //Override method
+        public override string ToString()
+        {
+            return this.Name + this.Diameter + this.Mass;
+        }
+    }
+}
---
+++ COMP123-Programming2-Assignment03/COMP123-Programming2-Assignment03/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * studentName:Bin Liang | studentNumber:300788322
+ * Date last Modified:Feb 24th,2015
+ * Program description:COMP123_Assignment03,Abstract Planets Class.
+ * Revision	History:
+ * 1.Finished Abstract Class Planet.
+ * 2.
+ * 3.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Programming2_Assignment03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
